71f500b7d0d1c4ffe654b17424843f85f10c129e
Ty <tylerstier13@gmail.com>
2025-04-26 16:13:26 -0700

    Parser PPTX to Json in Python

---
+++ backend/parser.py
@@ -0,0 +1,42 @@
+import zipfile
+import xml.etree.ElementTree as ET
+import json
+import os
+
+def pptx_to_json(pptx_file):
+    pptx_json = {
+        "slides": []
+    }
+
+    with zipfile.ZipFile(pptx_file, 'r') as pptx:
+        for filename in pptx.namelist():
+            if filename.startswith('ppt/slides/slide') and filename.endswith('.xml'):
+                slide_data = pptx.read(filename)
+                slide_json = parse_slide(slide_data)
+                pptx_json["slides"].append(slide_json)
+
+    return pptx_json
+
+def parse_slide(slide_data):
+    slide_json = {
+        "text": []
+    }
+    root = ET.fromstring(slide_data)
+
+    for element in root.iter():
+        if 't' in element.tag and element.text:
+            slide_json["text"].append(element.text)
+
+    return slide_json
+
+filepath = ''
+
+for file in os.listdir("../pptxfiles"):
+    if file.endswith(".pptx"):
+        filepath = os.path.join("../pptxfiles", file)
+        print(filepath)
+
+pptx_file = filepath
+pptx_json = pptx_to_json(pptx_file)
+
+print(json.dumps(pptx_json, indent=2))
